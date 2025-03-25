f71daa129a95f04014dadf82a71bdcaa75e83c5c
emad <emadi68moh@gmail.com>
2025-03-25 11:57:04 +0330

    feat(scripts): add git cliff tag automation script

---
+++ git.sh
@@ -0,0 +1,9 @@
+# Generate the JSON output from git-cliff
+output=$(git cliff --unreleased --bump --context)
+
+# Extract the version and changelog body using jq
+version=$(echo "$output" | jq -r '.[0].version')
+changelog=$(echo "$output" | jq -r '.[0].body')
+
+# Create an annotated Git tag with the changelog as the tag message
+git tag -a "$version" -m "$changelog"
